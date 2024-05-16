9e448f9a17dbfa9e4a75955bddbe8a5363e135e6
NaufalikhoAbyan <apradana448@gmail.com>
2024-05-16 20:03:25 +0700

    fix: Skill icons not adjusting to some mobile sizes

--- src/components/cards/SkillCard.vue
+++ src/components/cards/SkillCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="bg-secondary rounded-xl items-center gap-2 lg:gap-10 p-8 flex-col flex lg:grid lg:grid-cols-10">
-      <div class="h-16 md:h-32 lg:col-span-3">
+    <div class="bg-secondary h-full rounded-xl items-center gap-2 lg:gap-10 p-8 flex-col flex lg:grid lg:grid-cols-10">
+      <div class="flex items-center justify-center aspect-square h-16 md:h-28 lg:col-span-3">
         <slot name="icon"/>
       </div>
       <div class="flex flex-col justify-around h-full lg:col-span-7">
